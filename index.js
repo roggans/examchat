049785632030b859a69c9a6966c8a4edd6598182
roggans <rogge_acdc@hotmail.com>
2019-09-04 20:51:35 +0200

    commented out console.logs plus minor code cleanup

--- index.js
+++ index.js
@@ -16,50 +16,27 @@ app.get('/random', (req, res)=> {
 });
 
 io.on('connection', (socket) =>{
-  console.log('a user connected: socketid:' +socket.id);
+  // console.log('a user connected: socketid:' +socket.id);
   socket.on('join',(data)=>{
-    console.log(data)
+    // console.log(data)
     socket.join(data.room)
     io.in(data.room).emit('messageFromServer', `A User joined ${data.room} room!`)
   })
 
-  // socket.on('rumbyte',(room)=>{
-  //   console.log('vilket rum nu?:',room);
-  
-  //   socket.join(room,()=>{
-  //   console.log('rummet:',room)
-   
-
-//     socket.emit('messageFromServer',{
-//       room: room,
-//       msg: 'Hej i nya rummet'
-//     })
-//     io.to(room).emit('messageFromServer', 'User has joined')
-//   })  
-// })
-  // socket.on('leaveroom', (room)=>{
-  //   console.log('room was left',room)
-  //    socket.leave(room)
-  //    io.emit('messageFromServer', 'Socket left!')
-    
-  // })
-
     socket.on('fromClient',(data)=>{
-      console.log(data)
-     // room = data.room
-      console.log('fromClient msg:',data.msg) 
-      console.log('fromclient vilket rum',data.room)
+      // console.log(data)
+      // console.log('fromClient msg:',data.msg) 
+      // console.log('fromclient vilket rum',data.room)
           io.in(data.room).emit('messageFromServer',data.msg)
     })   
   
   
-  socket.on('disconnect', (data)=>{
-    console.log(data)
-    io.emit('messageFromServer','A user left'
-    )
-  });
+    socket.on('disconnect', (data)=>{
+      // console.log(data)
+      io.emit('messageFromServer','A user left')
+      });
     })
 
 http.listen(port, function(){
-  console.log('listening on *:' + port);
+  // console.log('listening on *:' + port);
 });
